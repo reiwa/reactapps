cb4766cffc44a189053db6f68376a730ad0027ff
reiwa <reiwa@outlook.com.vn>
2020-07-13 00:34:28 +0700

    add: lily

---
+++ lily/src/App.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import { interval } from 'rxjs'
+
+const App: FunctionComponent = () => {
+  const alphabets = [
+    `Á É Í Ó Ú Ý á é í ó ú ý`,
+    `Ä Ë Ï Ö Ü Ÿ ä ë ï ö ü ÿ`,
+    `Ç Ş ç ş`,
+    `Ã Õ ã õ`,
+  ]
+    .map(a => a.split(' '))
+    .flat()
+  const text = 'リリイ・シュシュのすべて'
+  const [state, setState] = useState([0, 6])
+
+  useEffect(() => {
+    const subscription = interval(20).subscribe(() => {
+      setState(([i, c]) => {
+        if (text.length - 2 < i && c === 1) {
+          subscription.unsubscribe()
+        }
+
+        return [c < 1 ? i + 1 : i, c < 1 ? 2 : c - 1]
+      })
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
+
+  const [index, count] = state
+
+  if (index === text.length - 1 && count === 0) {
+    return <p>{text}</p>
+  }
+
+  const a = alphabets[Math.floor(Math.random() * alphabets.length)]
+
+  return <p>{text.slice(0, index) + a}</p>
+}
+
+export default App
