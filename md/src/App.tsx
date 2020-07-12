3c2bcb6c678e5048b886d8eabd35478729b762b6
reiwa <reiwa@outlook.com.vn>
2020-07-13 00:34:42 +0700

    add: md

---
+++ md/src/App.tsx
@@ -0,0 +1,15 @@
+import marked from 'marked'
+import React, { Fragment, FunctionComponent, useState } from 'react'
+
+const App: FunctionComponent = () => {
+  const [text, setText] = useState('')
+
+  return (
+    <Fragment>
+      <textarea onChange={event => setText(event.target.value)} value={text} />
+      <div dangerouslySetInnerHTML={{ __html: marked(text) }} />
+    </Fragment>
+  )
+}
+
+export default App
