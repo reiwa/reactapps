bf0ab5800fd978c7b79bd7604e35ff1e1b420035
reiwa <reiwa@outlook.com.vn>
2020-07-13 00:33:05 +0700

    add: filter

---
+++ filter/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { Fragment, FunctionComponent, useState } from 'react'
+
+const items = new Array(240).fill(null).map(a =>
+  Math.random()
+    .toString(36)
+    .slice(-8)
+)
+
+const App: FunctionComponent = () => {
+  const [text, setText] = useState('')
+
+  return (
+    <Fragment>
+      <input onChange={event => setText(event.target.value)} value={text} />
+      <ul>
+        {items
+          .filter(item => item.includes(text))
+          .map(item => (
+            <li key={item}>{item}</li>
+          ))}
+      </ul>
+    </Fragment>
+  )
+}
+
+export default App
