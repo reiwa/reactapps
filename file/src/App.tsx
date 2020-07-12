a1ba06fda1dac2e70bcee9065319b2b28fad8f36
reiwa <reiwa@outlook.com.vn>
2020-07-13 00:32:51 +0700

    add: file

---
+++ file/src/App.tsx
@@ -0,0 +1,46 @@
+import React, {
+  Fragment,
+  FunctionComponent,
+  useState,
+  createRef,
+  ChangeEvent,
+} from 'react'
+
+const App: FunctionComponent = () => {
+  const [src, setSrc] = useState('')
+  const ref = createRef<HTMLInputElement>()
+  const onClick = () => {
+    if (ref.current) {
+      ref.current.click()
+    }
+  }
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files === null) {
+      return
+    }
+    const file = event.target.files.item(0)
+    if (file === null) {
+      return
+    }
+    var reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+      setSrc(reader.result as string)
+    }
+  }
+
+  return (
+    <Fragment>
+      <input
+        onChange={onChange}
+        ref={ref}
+        style={{ display: 'none' }}
+        type={'file'}
+      />
+      <button onClick={onClick}>file</button>
+      <div>{src && <img src={src} />}</div>
+    </Fragment>
+  )
+}
+
+export default App
